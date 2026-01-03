20cfe0061c6bf44b2ba6202b356c2502f5536bb2
mirdayan <dmirandaherrera@gmail.com>
2026-01-02 20:17:36 -0500

    subiendo el archivo javascript para dar interactividad al proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //Seleccionando los elementos de la interfax
+
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    
+    console.log(inputMensaje);
+
+
+});
