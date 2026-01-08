120efd6eeb1a18640618c2edc2c35f97428a4416
mirdayan <dmirandaherrera@gmail.com>
2026-01-07 22:05:05 -0500

    se genera la corrección de la validación de los campos del formulario.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    const inputEmailCC =document.querySelector('#email_cc')
+    const inputEmailCC = document.querySelector('#email_cc')
     
     // Asignar eventos
     // el evento blur se ejecuta al abandonar un campo a otro del formulario.
@@ -24,13 +24,13 @@ document.addEventListener('DOMContentLoaded', function() {
           mostrarAlerta(`El campo ${evento.target.id} es obligatorio`,evento.target.parentElement); // de forma dinámica mostramos la alerta según el campo seleccionado.
           return;
         }
-          if (evento.target.id === 'email' && 'email_cc' && !validarEmail(evento.target.value)) {
+          if ((evento.target.id === 'email' || evento.target.id === 'email_cc') && !validarEmail(evento.target.value)) {
             mostrarAlerta('El email no es válido', evento.target.parentElement);
             return;
 
           };
 
-        limpiarAlerta (evento.target.parentElement);
+          limpiarAlerta (evento.target.parentElement);
 
 
 
@@ -41,8 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
       limpiarAlerta(referencia);
 
 
-    
-
+  
 
        // Generar alerta en HTML
       const error = document.createElement('p'); 
@@ -54,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
      }   
     
+
 
     function limpiarAlerta(referencia) {
 
