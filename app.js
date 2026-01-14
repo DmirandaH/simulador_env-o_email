e7c3b62e0b5fe43adb0cb718814d52bd82084288
mirdayan <dmirandaherrera@gmail.com>
2026-01-13 22:17:47 -0500

    Agreando las funcionalidades de habilitar y deshabilitar el botón de envío y reseteando el formulario.

--- app.js
+++ app.js
@@ -3,44 +3,61 @@ document.addEventListener('DOMContentLoaded', function() {
   const email = {
     email: '',
     asunto: '',
-    emailcc: '',
+    email_cc: '',
     mensaje: ''
 
   }
 
+  console.log(email);
 
-
-
-
-    
+   
     //Seleccionando los elementos de la interfax
 
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    const inputEmailCC = document.querySelector('#email_cc')
+    const inputEmailCC = document.querySelector('#email_cc');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
     
     // Asignar eventos
     // el evento blur se ejecuta al abandonar un campo a otro del formulario.
+    // input me permite ocultar al alerta en tiempo real.
 
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
-    inputEmailCC.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
+    inputEmailCC.addEventListener('input', validar);
+
+    btnReset.addEventListener('click', function(evento) {
+
+      evento.preventDefault();
+
+      // reinciar el objeto
+      email.email = '',
+      email_cc.email_cc = '',
+      asunto.asunto = '',
+      mensaje.mensaje = '';
+
+      formulario.reset();
+      comprobarEmail();
+
+    })
 
 
     function validar (evento) { 
-
-
-   
-     
+ 
         if (evento.target.value.trim() === '') {
           mostrarAlerta(`El campo ${evento.target.id} es obligatorio`,evento.target.parentElement); // de forma dinámica mostramos la alerta según el campo seleccionado.
+          email[evento.target.name] = '';
+          comprobarEmail();
           return;
         }
           if ((evento.target.id === 'email' || evento.target.id === 'email_cc') && !validarEmail(evento.target.value)) {
             mostrarAlerta('El email no es válido', evento.target.parentElement);
+            email[evento.target.name] = '';
+            comprobarEmail();
             return;
 
           };
@@ -49,13 +66,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
           // Asignar los valores
 
-          email [evento.target.name] = evento.target.value.trim().toLowerCase();
+          email[evento.target.name] = evento.target.value.trim().toLowerCase();
 
      
           //Comprobar el obejto de email
           comprobarEmail();
-
-
 
 
     }
@@ -77,8 +92,6 @@ document.addEventListener('DOMContentLoaded', function() {
   
      }   
     
-
-
     function limpiarAlerta(referencia) {
 
     // Comprueba si ya existe una alerta
@@ -98,8 +111,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     function comprobarEmail() {
-      console.log(Object.values(email).includes('') );
+       if(Object.values(email).includes('')) {
+           btnSubmit.classList.add('opacity-50');
+           btnSubmit.disabled = true;
+           return
 
-    }
+       }  btnSubmit.classList.remove('opacity-50');
+          btnSubmit.disabled = false;
+
+    }  
 
 });
